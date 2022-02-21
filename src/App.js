140b3e19e0f574c40d42a2648d5f348351178264
Etem Senel <etemsenel96@hotmail.de>
2022-02-22 02:07:20 +0300

    dynamic routers were created for each coins. footer were also added but without styling.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
   return (
     <Router>
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cryptocurrencies" element={<CryptoCurrencies />} />
